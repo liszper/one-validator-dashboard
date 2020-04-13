cceead926c7e7d4f1298541dcd506010dc2736c8
R.A.Z <raz@zgen.hu>
2020-04-13 20:38:06 +0200

    hotfix

--- run-node.sh
+++ run-node.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd ~
 KEY=$(find . -maxdepth 1 -type f -iname "*.key" | head -1)
-~/node.sh -S -c -z -I -N staking -k $KEY -p "secret"
+PASS=$(find . -maxdepth 1 -type f -iname "*.pass" | head -1)
+./node.sh -S -c -z -I -N staking -k $KEY -p $PASS
--- run.sh
+++ run.sh
@@ -11,8 +11,10 @@ echo "
 "
 apt-get update && apt-get install git lolcat tmux leiningen -y && git clone https://github.com/liszper/one-validator-dashboard.git
 curl -LO https://harmony.one/hmycli && mv hmycli hmy && chmod +x hmy
-echo "12345" > secret
-./hmy keys generate-bls-key --passphrase-file secret
+KEY=$(find . -maxdepth 1 -type f -iname "*.key" | head -1)
+NAME="$(basename -- $KEY)"
+echo "12345" > "$NAME.pass"
+./hmy keys generate-bls-key --passphrase-file secret.pass
 curl -LO https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/node.sh && chmod a+x node.sh
 cd one-validator-dashboard
 ./install.sh
