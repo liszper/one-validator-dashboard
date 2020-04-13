cd22c515f66ba0b2a34187199271a16f7ed7b0eb
R.A.Z <raz@zgen.hu>
2020-04-13 20:21:59 +0200

    passphrase fix

--- run-node.sh
+++ run-node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 KEY=$(find . -maxdepth 1 -type f -iname "*.key" | head -1)
-./node.sh -S -c -z -I -N staking -k $KEY
+./node.sh -S -c -z -I -N staking -k $KEY --passphrase-file secret.key
--- run.sh
+++ run.sh
@@ -11,7 +11,8 @@ echo "
 "
 apt-get update && apt-get install git lolcat tmux leiningen -y && git clone https://github.com/liszper/one-validator-dashboard.git
 curl -LO https://harmony.one/hmycli && mv hmycli hmy && chmod +x hmy
-./hmy keys generate-bls-key
+echo "12345" > secret.key
+./hmy keys generate-bls-key --passphrase-file secret.key
 curl -LO https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/node.sh && chmod a+x node.sh
 cd one-validator-dashboard
 ./install.sh
