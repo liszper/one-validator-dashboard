df6767ca72ccfd7563ea193ec20920f59ff34da8
R.A.Z <raz@zgen.hu>
2020-04-13 20:48:04 +0200

    finalize

--- run.sh
+++ run.sh
@@ -10,11 +10,45 @@ echo "
 .'______________________________\\|/______________________________\`.
 "
 apt-get update && apt-get install git lolcat tmux leiningen -y && git clone https://github.com/liszper/one-validator-dashboard.git
+echo "                     ,
+                    /|
+                   /*|
+                  /.+|
+                 /* .|
+                 |:+.:V
+                 /+.:*.V
+                |:.*.:+|
+               /+.---.Z|
+             ,(((/o^o/))V
+            (())) . > ()))
+            )(())(/~_))))(
+           /((())())((()(/)
+          /::)).&& (()))():@
+         /:*::)'||.   /|+:::@
+        /:::::( || V / |:/:::)
+        V:::+/-'L|, &  |::*:/
+         |::(|_  _'   _V+::|
+         |*::V '-'   //,):/Ä
+         |:::+| ||
+		   " | lolcat
 curl -LO https://harmony.one/hmycli && mv hmycli hmy && chmod +x hmy
 KEY=$(find . -maxdepth 1 -type f -iname "*.key" | head -1)
 NAME="$(basename -- $KEY)"
-echo "12345" > "$NAME.pass"
+echo "I don't see the point" > "$NAME.pass"
 ./hmy keys generate-bls-key --passphrase-file secret.pass
 curl -LO https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/node.sh && chmod a+x node.sh
 cd one-validator-dashboard
-./install.sh
+echo "
+_______________________________________________________________________
+|[] AmigaShell                                                    |F]|!\"|
+|\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|\"|
+|12.Workbench:> cd harmony:one                                        | |
+|12.Work:Harmony/ONE> ed run-node.asc                                 | |
+|                                                                     | |
+|                                                                     |_|
+|_____________________________________________________________________|/|
+" | lolcat
+tmux new-session -d -s "nodeSession" ./run-node.sh
+tmux new-session -d -s "dashboardSession" ./install.sh
+echo "Navigate to the your.ip:3000 address in your favorite browser!"
+echo "You done it!"
