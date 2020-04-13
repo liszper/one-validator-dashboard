93c51f265d22f7429f8c031386b8a975ebd7c493
R.A.Z <raz@zgen.hu>
2020-04-13 20:14:32 +0200

    hotfix

--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ echo "
   .'//______.============-..   \\ | /   ..-============.______\\\\\`.
 .'______________________________\\|/______________________________\`.
 "
-apt-get update && apt-get install git lolcat tmux leiningen -y && git clone https://github.com/liszper/one-validator-dashboard.git && cd one-validator-dashboard
+apt-get update && apt-get install git lolcat tmux leiningen -y && git clone https://github.com/liszper/one-validator-dashboard.git
 curl -LO https://harmony.one/hmycli && mv hmycli hmy && chmod +x hmy
 ./hmy keys generate-bls-key
 curl -LO https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/node.sh && chmod a+x node.sh
+cd one-validator-dashboard
 ./install.sh
